79d3e854f0c215d00f289a5252996b5ff74dbe97
Zvonimir Sabljic <zvonimir@pythagora.io>
2023-02-03 09:14:22 +0100

    Improved handling app errors

--- RunPytagora.js
+++ RunPytagora.js
@@ -6,20 +6,24 @@ const Pytagora = require('./Pytagora.js');
 global.Pytagora = new Pytagora(mode);
 
 (async () => {
+    let app;
     await global.Pytagora.runRedisInterceptor();
     try {
         console.log(path.join(process.cwd(), initScript));
-        const app = require(path.join(process.cwd(), initScript));
-
-        if (mode === 'test') {
-            // TODO run tests once the app loads
-            console.log('Running tests in 3 seconds...');
-            setTimeout(() => {
-                require('./RunPytagoraTests.js');
-            }, 3000);
-        }
+        app = require(path.join(process.cwd(), initScript));
     } catch (e) {
-        console.error('Pytagora came across error: ', e);
+        console.error('The app has crashed!');
+        console.error('This is likely not related to Pytagora, but the app itself.');
+        console.error(e);
+        process.exit(1);
+    }
+
+    if (mode === 'test') {
+        // TODO run tests once the app loads
+        console.log('Running tests in 3 seconds...');
+        setTimeout(() => {
+            require('./RunPytagoraTests.js');
+        }, 3000);
     }
 })();
 

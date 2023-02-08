c6125f0f9524af7bbb8fdddebfaa1b2eb7a6d958
Zvonimir Sabljic <zvonimir@pythagora.io>
2023-02-08 19:51:36 +0100

    Removed extra LICENSE file + changed logs

--- src/utils/cmdPrint.js
+++ src/utils/cmdPrint.js
@@ -2,6 +2,7 @@ let { cutWithDots, getOccurrenceInArray } = require('./common');
 let pythagoraErrors = require('../const/errors.json');
 
 let red = '\x1b[31m',
+    yellow = '\x1b[33m',
     green = '\x1b[32m',
     blue = '\x1b[34m',
     reset = '\x1b[0m',
@@ -23,14 +24,14 @@ let logEndpointCaptured = (endpoint, method, body, query, responseBody = {}) => 
 
 let logEndpointNotCaptured = (endpoint, method, error) => {
     console.log(`
-❌ ${red+bold}${method} ${endpoint} ${reset}NOT captured because of: ${red+bold}${error}${reset}
+⚠️  ${yellow+bold}${method} ${endpoint} ${reset}NOT captured because of: ${yellow+bold}${error}${reset}
 `);
 }
 
 let logTestFailed = (test, response, pythagora) => {
     let errLog = '';
     for (const err in pythagoraErrors) {
-        errLog += `\t${pythagoraErrors[err] + ' : ' + getOccurrenceInArray(pythagora.request.errors, pythagoraErrors[err]) + '\n'}`
+        if (getOccurrenceInArray(pytagora.request.errors, pytagoraErrors[err]) > 0) errLog += `\t${pytagoraErrors[err]}\n`;
     }
     console.log(`❌ Test ${red+bold}FAILED!${reset}
     ${red+bold}${test.method} ${test.endpoint} ${reset}
@@ -66,7 +67,7 @@ let logCaptureFinished = (passed, failed, linesExecuted = undefined, codeCoverag
 ${blue+bold}************************************************************${reset}
 ${green+bold}Pythagora finished capturing!${reset}
 ${green+bold}${passed} ${reset}requests are ${green+bold}captured!${reset}
-Unable to capture ${red+bold}${failed} ${reset}request${failed === 1 ? '' : 's'}.${failed > 0 ? ' This is likely due to features Pythagora doesn\'t support yet like handling random variables (passwords, hashes, etc.), uploading files or some authentification methods.' : ''}
+Unable to capture ${yellow+bold}${failed} ${reset}request${failed === 1 ? '' : 's'}.${failed > 0 ? ' This is likely due to features Pythagora doesn\'t support yet like handling random variables (passwords, hashes, etc.), uploading files or some authentification methods.' : ''}
 ${blue+bold}************************************************************${reset}
     `);
 }

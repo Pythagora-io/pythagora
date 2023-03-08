c28de65c8a1bb0e67dc7dc87af30fa3849d696f4
LeonOstrez <leon.ostrez@gmail.com>
2023-03-08 15:08:18 +0100

    add support for session authentication

--- src/patches/express.js
+++ src/patches/express.js
@@ -12,9 +12,24 @@ removePythagoraMiddlewares = function (express) {
 pythagoraExpress = function () {
     const app = originalExpress.apply(this, arguments);
     const originalListen = app.listen;
+    const originalUse = app.use;
     app.listen = function () {
         global.Pythagora.expressInstances.filter(e => e !== this).forEach(removePythagoraMiddlewares);
         return originalListen.apply(this, arguments);
+    }
+    app.use = function() {
+        if (!global.Pythagora.authenticationMiddleware) return originalUse.apply(this, arguments);
+
+        let originalMiddleware = arguments[0];
+        arguments[0] = async function() {
+            global.Pythagora.authenticationInProcess = true;
+            let res = await originalMiddleware.apply(this, arguments);
+            global.Pythagora.authenticationInProcess = false;
+            return res;
+        };
+
+        global.Pythagora.authenticationMiddleware = false;
+        return originalUse.apply(this, arguments);
     }
     if (global.Pythagora) global.Pythagora.setApp(app);
     return app;

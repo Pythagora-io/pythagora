9fd3e7eb73901efd9f1dd094859fadbbc9cc2077
Zvonimir Sabljic <zvonimir@pythagora.io>
2023-03-09 17:57:46 +0100

    Continuously check for finishingup file and exit when it appears + setup for if we need to manually kill the hanging process to trigger nyc or other exit processing

--- bin/run.bash
+++ bin/run.bash
@@ -3,12 +3,27 @@ args=("$@")
 script_path=$(realpath $0)
 init_command=
 init_script=
+APP_PID=
 mode="capture"
 yellow=$(tput setaf 3)
 green=$(tput setaf 2)
 reset=$(tput sgr0)
 bold=$(tput bold)
 pythagora_dir=$(basename $(dirname $(dirname $script_path)))
+
+function exit_handler {
+  while [ ! -f "./.pythagora/finishingup" ]
+  do
+    sleep .5
+  done
+
+  rm -f "./.pythagora/finishingup"
+#  kill -s SIGINT $1
+#  sleep .5
+  exit 0
+}
+
+
 if [ "$pythagora_dir" == "pythagora-dev" ]
 then
   pythagora_dir="/@pythagora.io/pythagora-dev"
@@ -28,27 +43,11 @@ do
   fi
 done
 
-function exit_handler() {
-  while [ ! -f "./.pythagora/finishingup" ]
-  do
-    echo "Waiting for finishingup file to appear..."
-    sleep .5
-  done
-
-  rm -f "./.pythagora/finishingup"
-  exit 0
-}
-
-if [[ "${mode}" == "capture" ]]
-then
-  trap exit_handler EXIT
-fi
-
 
 if [[ " ${args[@]} " =~ " --no-code-coverage " ]] || ([[ ! " ${args[@]} " =~ " --mode test " ]] && [[ ! " ${args[@]} " =~ " --mode=test " ]])
 then
   args=( "${args[@]//--no-code-coverage/}" )
-  PYTHAGORA_MODE="$mode" NODE_OPTIONS="--inspect --require ./node_modules/${pythagora_dir}/RunPythagora.js" $init_command
+  PYTHAGORA_MODE="$mode" NODE_OPTIONS="--require ./node_modules/${pythagora_dir}/RunPythagora.js" $init_command
 else
   nyc_args=( "--reporter=text-summary" )
 
@@ -61,11 +60,13 @@ else
 
   if [ -f "./node_modules/$pythagora_dir/node_modules/nyc/bin/nyc.js" ]
   then
-    PYTHAGORA_MODE="$mode" NODE_OPTIONS="--inspect --require ./node_modules/${pythagora_dir}/RunPythagora.js" ./node_modules/"$pythagora_dir"/node_modules/nyc/bin/nyc.js "${nyc_args[@]}" $init_command
-#    ./node_modules/"$pythagora_dir"/node_modules/nyc/bin/nyc.js "${nyc_args[@]}" node ./node_modules/"$pythagora_dir"/RunPythagora.js "${args[@]}"
+    PYTHAGORA_MODE="$mode" NODE_OPTIONS="--inspect --require ./node_modules/${pythagora_dir}/RunPythagora.js" ./node_modules/"$pythagora_dir"/node_modules/nyc/bin/nyc.js "${nyc_args[@]}" $init_command &
   else
-    PYTHAGORA_MODE="$mode" NODE_OPTIONS="--inspect --require ./node_modules/$pythagora_dir/RunPythagora.js" ./node_modules/nyc/bin/nyc.js "${nyc_args[@]}" $init_command
-#     ./node_modules/nyc/bin/nyc.js "${nyc_args[@]}" node ./node_modules/"$pythagora_dir"/RunPythagora.js "${args[@]}"
+    PYTHAGORA_MODE="$mode" NODE_OPTIONS="--inspect --require ./node_modules/${pythagora_dir}/RunPythagora.js" ./node_modules/nyc/bin/nyc.js "${nyc_args[@]}" $init_command &
   fi
 
 fi
+
+APP_PID=$!
+
+exit_handler $APP_PID

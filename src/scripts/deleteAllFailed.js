b7688263bd6ea316a7bb750e5bebc4a554499803
LeonOstrez <leon.ostrez@gmail.com>
2023-04-21 11:17:56 +0200

    cleanup review.json after deleting all failed tests

--- src/scripts/deleteAllFailed.js
+++ src/scripts/deleteAllFailed.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { PYTHAGORA_METADATA_DIR, PYTHAGORA_TESTS_DIR, METADATA_FILENAME, PYTHAGORA_DELIMITER } = require('../const/common.js');
+const { REVIEW_DATA_FILENAME, PYTHAGORA_METADATA_DIR, PYTHAGORA_TESTS_DIR, METADATA_FILENAME, PYTHAGORA_DELIMITER } = require('../const/common.js');
 const { getCircularReplacer } = require('../utils/common.js');
 const { logAndExit } = require('../utils/cmdPrint.js');
 
@@ -11,13 +11,20 @@ if (!metadata || !metadata.runs || !metadata.runs.length ||
 let deleteTests = metadata.runs[metadata.runs.length - 1].failed;
 let files = fs.readdirSync(`./${PYTHAGORA_TESTS_DIR}/`);
 
-files = files.filter(f => f[0] !== '.');
-for (let file of files) {
-    if (file.indexOf(PYTHAGORA_DELIMITER) !== 0) continue;
-    let tests = JSON.parse(fs.readFileSync(`./${PYTHAGORA_TESTS_DIR}/${file}`));
-    let newTests = tests.filter((t) => !deleteTests.includes(t.id));
+try {
+    files = files.filter(f => f[0] !== '.');
+    for (let file of files) {
+        if (file.indexOf(PYTHAGORA_DELIMITER) !== 0) continue;
+        let tests = JSON.parse(fs.readFileSync(`./${PYTHAGORA_TESTS_DIR}/${file}`));
+        let newTests = tests.filter((t) => !deleteTests.includes(t.id));
 
-    if (tests.length !== newTests.length) fs.writeFileSync(`./${PYTHAGORA_TESTS_DIR}/${file}`, JSON.stringify(newTests, getCircularReplacer(), 2));
+        if (tests.length !== newTests.length) fs.writeFileSync(`./${PYTHAGORA_TESTS_DIR}/${file}`, JSON.stringify(newTests, getCircularReplacer(), 2));
+    }
+
+    //cleanup review.json if all failed tests are deleted
+    fs.writeFileSync(`./${PYTHAGORA_METADATA_DIR}/${REVIEW_DATA_FILENAME}`, '[]');
+
+    logAndExit('Successfully deleted all failed tests!', 'log');
+} catch (e) {
+    logAndExit(`Error deleting all failed tests: ${e.message}`, 'error');
 }
-
-logAndExit('Successfully deleted all failed tests!', 'log');

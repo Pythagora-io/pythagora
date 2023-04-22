2abe2a1425198312eca4d895cbff6ece24acfd4e
Zvonimir Sabljic <zvonimir@pythagora.io>
2023-04-22 12:17:04 +0200

    Added Jest mode + small refactor so that modes are stored in only one place

--- src/utils/argumentsCheck.js
+++ src/utils/argumentsCheck.js
@@ -1,4 +1,4 @@
-const AVAILABLE_MODES = ['capture', 'test'];
+const AVAILABLE_MODES = Object.keys(require('../const/modes.json'));
 const argsStr = process.env.PYTHAGORA_CONFIG;
 const { deleteTest } = require('../helpers/starting.js');
 const { logAndExit } = require('./cmdPrint.js');
@@ -28,7 +28,7 @@ if (!args.mode) {
 }
 
 if (args.rerun_all_failed && args.mode !== 'test') logAndExit(`Flag --rerun_all_failed allowed only in "--mode test"`);
-if (args.test_id && args.mode !== 'test') logAndExit(`Flag --test-id allowed only in "--mode test"`);
+if (args.test_id && !(args.mode === 'test' || args.mode === 'jest')) logAndExit(`Flag --test-id allowed only in "--mode test"`);
 if (args.rerun_all_failed && args.test_id) logAndExit(`Not allowed to set flags --rerun_all_failed and --test-id at same time.`);
 
 if (args.pick && args.mode !== 'capture') logAndExit(`Flag --pick allowed only in "--mode capture"`);

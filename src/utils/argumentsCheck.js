c8775db9f95e42200f7dcde9a06ee523e677a720
Zvonimir Sabljic <zvonimir@pythagora.io>
2023-04-28 09:05:43 +0200

    Added export mode default and changed all hardcoded modes to use the ones from modes.json

--- src/utils/argumentsCheck.js
+++ src/utils/argumentsCheck.js
@@ -1,4 +1,5 @@
-const AVAILABLE_MODES = Object.keys(require('../const/modes.json'));
+const MODES = require('../const/modes.json');
+const AVAILABLE_MODES = Object.keys(MODES);
 const argsStr = process.env.PYTHAGORA_CONFIG;
 const { deleteTest } = require('../helpers/starting.js');
 const { logAndExit } = require('./cmdPrint.js');
@@ -16,23 +17,25 @@ for (let i = 0; i < argArray.length; i++) {
 if (args.delete) deleteTest(args.delete);
 
 if (!args.mode) {
-    if (args.rerun_all_failed || args.test_id) {
+    if (args.export === 'deep') {
+        args.mode = MODES.test;
+    } else if (args.rerun_all_failed || args.test_id) {
         console.log('Mode not provided. Setting to "test".');
-        args.mode = 'test';
+        args.mode = MODES.test;
     } else {
         console.log('Mode not provided. Defaulting to "capture".');
-        args.mode = 'capture';
+        args.mode = MODES.capture;
     }
 } else if (!AVAILABLE_MODES.includes(args.mode)) {
     logAndExit(`Mode "${args.mode}" not recognized. Available modes are: ${AVAILABLE_MODES.join(', ')}`);
 }
 
-if (args.rerun_all_failed && args.mode !== 'test') logAndExit(`Flag --rerun_all_failed allowed only in "--mode test"`);
-if (args.test_id && !(args.mode === 'test' || args.mode === 'jest')) logAndExit(`Flag --test-id allowed only in "--mode test"`);
+if (args.rerun_all_failed && args.mode !== MODES.test) logAndExit(`Flag --rerun_all_failed allowed only in "--mode test"`);
+if (args.test_id && !(args.mode === MODES.test || args.mode === MODES.jest)) logAndExit(`Flag --test-id allowed only in "--mode test"`);
 if (args.rerun_all_failed && args.test_id) logAndExit(`Not allowed to set flags --rerun_all_failed and --test-id at same time.`);
 
-if (args.pick && args.mode !== 'capture') logAndExit(`Flag --pick allowed only in "--mode capture"`);
-if (args.ignore && args.mode !== 'capture') logAndExit(`Flag --ignore allowed only in "--mode capture"`);
+if (args.pick && args.mode !== MODES.capture) logAndExit(`Flag --pick allowed only in "--mode capture"`);
+if (args.ignore && args.mode !== MODES.capture) logAndExit(`Flag --ignore allowed only in "--mode capture"`);
 
 console.log(`Running "${process.env.PYTHAGORA_CONFIG}" using Pythagora in "${args.mode.toUpperCase()}" mode.`);
 

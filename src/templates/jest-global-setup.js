eee35d4cc0263372c94ac9578729b3559bf99a5d
Adarsh Jha <132337675+adarsh-jha-dev@users.noreply.github.com>
2023-10-23 10:23:35 +0530

    Remove unnecessary if block in jest-global-setup.js

--- src/templates/jest-global-setup.js
+++ src/templates/jest-global-setup.js
@@ -1,10 +1,5 @@
 module.exports = async () => {
     // Check if the developer has opted out of telemetry and set the environment variable accordingly
     const optOutTelemetry = process.env.PYTHAGORA_TELEMETRY_ENABLED === 'false';
-
-    if (optOutTelemetry) {
-        process.env.PYTHAGORA_TELEMETRY_ENABLED = 'false';
-    }
-
     console.log(`Telemetry is ${optOutTelemetry ? 'disabled' : 'enabled'}`);
 };
